e71139d331f51bdaad9c7cbd1c9ad97140ee5b20
asgabrielsilva <gabrielalmeida290905@gmail.com>
2023-05-19 11:59:13 -0300

    Inicio do Projeto

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { livros } from '@/_data/livros.js'
+</script>
+
+<template>
+  <h1>Livros disponiveis</h1>
+  <div class="listagem-livros">
+    <div class="card-livro" v-for="livro in livros" :key="livro.id">
+      <img :src="livro.img" alt="Capa do livro" class="capa-livro" />
+      <p class="titulo-livro">Título: {{ livro.title }}</p>
+      <p class="autor-livro">Autor: {{ livro.author }}</p>
+      <p class="preco-livro">Preço: R${{ livro.price.toFixed(2) }}</p>
+    </div>
+  </div>
+  <!-- {{ livros }} -->
+</template>
+
+<style scoped>
+.listagem-livros {
+  display: flex;
+  flex-wrap: wrap;
+}
+.capa-livro {
+  width: 200px;
+}
+.card-livro p {
+  margin: 0;
+}
+.card-livro .titulo-livro {
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+.card-livro {
+  margin: 5px 10px;
+  padding: 10px;
+  background-color: beige;
+  border-radius: 10px;
+}
+</style>
---
+++ src/_data/livros.js
@@ -0,0 +1,44 @@
+export const livros = [
+    {
+      id: 1,
+      title: 'O Poderoso Chefão',
+      author: 'Mario Puzo',
+      price: 49.9,
+      img: 'https://m.media-amazon.com/images/I/41i-ffd-iFL._SX335_BO1,204,203,200_.jpg'
+    },
+    {
+      id: 2,
+      title: 'Senhor dos Anéis',
+      author: 'J. R. R. Tolkien',
+      price: 39.9,
+      img: 'https://m.media-amazon.com/images/I/41i-ffd-iFL._SX335_BO1,204,203,200_.jpg'
+    },
+    {
+      id: 3,
+      title: 'Senhora',
+      author: 'José de Alencar',
+      price: 29.9,
+      img: 'https://m.media-amazon.com/images/I/41i-ffd-iFL._SX335_BO1,204,203,200_.jpg'
+    },
+    {
+      id: 4,
+      title: 'Turma da Monica',
+      author: 'Mauricio de Souza',
+      price: 9.9,
+      img: 'https://m.media-amazon.com/images/I/41i-ffd-iFL._SX335_BO1,204,203,200_.jpg'
+    },
+    {
+      id: 5,
+      title: 'Bíblia',
+      author: 'Deus',
+      price: 1.9,
+      img: 'https://m.media-amazon.com/images/I/41i-ffd-iFL._SX335_BO1,204,203,200_.jpg'
+    },
+    {
+      id: 6,
+      title: 'O Conde de Monte Cristo',
+      author: 'Alexandre Dumas',
+      price: 69.9,
+      img: 'https://m.media-amazon.com/images/I/41i-ffd-iFL._SX335_BO1,204,203,200_.jpg'
+    }
+  ]
